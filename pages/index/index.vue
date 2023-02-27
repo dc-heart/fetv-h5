563e71fcefd79f187795693d497390c941fd3f51
dc-heart <2877456987@qq.com>
2023-02-27 15:08:57 +0800

    头部组件的更新

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<base-head>
+			<head-search></head-search>
+		</base-head>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
